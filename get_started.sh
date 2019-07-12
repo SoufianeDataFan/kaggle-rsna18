74f0f8abf31cbd3ba72c904439e3635226741b8c
CHAMI Soufiane <22435134+SoufianeDataFan@users.noreply.github.com>
2019-07-12 15:09:00 +0100

    Update get_started.sh

--- get_started.sh
+++ get_started.sh
@@ -1,9 +1,5 @@
 cd models/RetinaNet/ ; pip install .
-
-
 cd -
-
-
 mkdir data ; cd data 
 # Download the challenge data here 
 kaggle competitions download -c rsna-pneumonia-detection-challenge
@@ -12,5 +8,5 @@ unzip stage_2_train_labels.csv.zip
 mv stage_2_detailed_class_info.csv detailed_class_info.csv 
 mv stage_2_train_labels.csv train_labels.csv 
 mkdir train_dicoms test_dicoms 
-cd train_dicoms ; unzip ../stage_2_train_images.zip 
-cd ../test_dicoms ; unzip ../stage_2_test_images.zip 
+cd train_dicoms ; unzip -qq ../stage_2_train_images.zip 
+cd ../test_dicoms ; unzip -qq ../stage_2_test_images.zip 
