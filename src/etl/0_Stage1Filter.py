440da0c6b078ceae190caf32ad9b49ff81d113ab
CHAMI Soufiane <22435134+SoufianeDataFan@users.noreply.github.com>
2019-07-12 13:51:14 +0100

    Update 0_Stage1Filter.py

--- src/etl/0_Stage1Filter.py
+++ src/etl/0_Stage1Filter.py
@@ -17,6 +17,7 @@ with open(os.path.join(WDIR, "stage_1_patientIds.txt")) as f:
     stage_1_patientIds = [line.strip() for line in stage_1_patientIds]
 
 path_to_labels_file = os.path.join(WDIR, "../../", SETTINGS_JSON["TRAIN_INFO_DIR"], "detailed_class_info.csv")
+print(path_to_labels_file)
 labels_df = pd.read_csv(path_to_labels_file) 
 labels_df = labels_df[labels_df.patientId.isin(stage_1_patientIds)]
 labels_df.to_csv(path_to_labels_file, index=False) 
@@ -24,4 +25,4 @@ labels_df.to_csv(path_to_labels_file, index=False)
 path_to_boxes_file = os.path.join(WDIR, "../../", SETTINGS_JSON["TRAIN_INFO_DIR"], "train_labels.csv")
 boxes_df = pd.read_csv(path_to_boxes_file) 
 boxes_df = boxes_df[boxes_df.patientId.isin(stage_1_patientIds)]
-boxes_df.to_csv(path_to_boxes_file, index=False) 
+boxes_df.to_csv(path_to_boxes_file, index=False)
