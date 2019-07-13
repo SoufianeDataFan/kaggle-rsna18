a790fbb66d7335c181351bac46b21216a52b3a26
CHAMI Soufiane <22435134+SoufianeDataFan@users.noreply.github.com>
2019-07-13 03:02:47 +0100

    Update prepare_data.sh

--- prepare_data.sh
+++ prepare_data.sh
@@ -1,21 +1,21 @@
 echo "Running src/etl/0_Stage1Filter.py  ..."
-python src/etl/0_Stage1Filter.py
+python -W ignore src/etl/0_Stage1Filter.py
 echo "Extracting metadata from DICOM Files ..."
-python src/etl/1_GetMetadata.py
+python -W ignore src/etl/1_GetMetadata.py
 echo "Creating stratified 10-fold cross-validation ..."
-python src/etl/2_AssignCVFolds.py
+python -W ignore src/etl/2_AssignCVFolds.py
 echo "Converting DICOMs to PNGs ..." 
-python src/etl/3_ConvertDICOM2PNG.py 
+python -W ignore src/etl/3_ConvertDICOM2PNG.py 
 echo "Creating resized numpy arrays from PNGs ..."
-python src/etl/4_CreateResizedNumpyArrays.py
+python -W ignore src/etl/4_CreateResizedNumpyArrays.py
 echo "Creating flipped test images for later inference ..."
-python src/etl/5_FlipTestImages.py
+python -W ignore src/etl/5_FlipTestImages.py
 echo "Transforming data into COCO format ..."
-python src/etl/6_COCOify.py train
-python src/etl/6_COCOify.py test
+python -W ignore src/etl/6_COCOify.py train
+python -W ignore src/etl/6_COCOify.py test
 echo "Generating concatenated images for ConcatRetinaNet ..."
-python src/etl/7_CreateConcatImages.py 
+python -W ignore src/etl/7_CreateConcatImages.py 
 echo "Generating annotations for training keras-retinanet ..."
-python src/etl/8_GenerateConcatRetinaAnnotations.py 
-python src/etl/9_GeneratePositiveRetinaAnnotations.py
+python -W ignore src/etl/8_GenerateConcatRetinaAnnotations.py 
+python -W ignore src/etl/9_GeneratePositiveRetinaAnnotations.py
 echo "DONE !" 
