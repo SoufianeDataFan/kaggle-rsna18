482dc3336ac9a1b3263b5e6f45fb7fa491a8856f
CHAMI Soufiane <22435134+SoufianeDataFan@users.noreply.github.com>
2019-07-13 03:03:25 +0100

    Update 4_CreateResizedNumpyArrays.py

--- src/etl/4_CreateResizedNumpyArrays.py
+++ src/etl/4_CreateResizedNumpyArrays.py
@@ -58,7 +58,6 @@ def resize_images_and_save_as_nparray(list_of_images, in_dir, out_dir, new_size=
         sys.stdout.flush()
         in_dir= str(in_dir)
         img= img.decode('ascii')
-        print(os.path.join(in_dir, img))
         loaded_img = scipy.misc.imread(os.path.join(in_dir, img), mode="L")
         resized_img = resize_image(loaded_img, new_size) 
         np.save(os.path.join(out_dir, img.replace("png", "npy")), resized_img) 
