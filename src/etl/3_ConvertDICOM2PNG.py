ffc36c7c8863554b809b462919a33bab3f804a3b
CHAMI Soufiane <22435134+SoufianeDataFan@users.noreply.github.com>
2019-07-13 15:54:41 +0100

    Parallelized version

--- src/etl/3_ConvertDICOM2PNG.py
+++ src/etl/3_ConvertDICOM2PNG.py
@@ -37,22 +37,45 @@ if not os.path.exists(train_images_dir): os.makedirs(train_images_dir)
 
 if not os.path.exists(test_images_dir):  os.makedirs(test_images_dir) 
 
-for i, each_train_dicom in enumerate(list_of_train_dicoms): 
-    sys.stdout.write("DICOM to PNG conversion: {}/{} ...\r".format(i+1, len(list_of_train_dicoms)))
-    sys.stdout.flush() 
-    tmp_dicom_array = pydicom.read_file(each_train_dicom).pixel_array
-    assert np.min(tmp_dicom_array) >= 0 & np.max(tmp_dicom_array) <= 255
-    scipy.misc.imsave(os.path.join(train_images_dir, each_train_dicom.split("/")[-1].replace("dcm", "png")),
-                      tmp_dicom_array)
+    
+def dicom_to_png_conversion_train(list_of_train_dicoms):
+    
+    for i, each_train_dicom in enumerate(list_of_train_dicoms): 
+        sys.stdout.write("DICOM to PNG conversion: {}/{} ...\r".format(i+1, len(list_of_train_dicoms)))
+        sys.stdout.flush() 
+        tmp_dicom_array = pydicom.read_file(each_train_dicom).pixel_array
+        assert np.min(tmp_dicom_array) >= 0 & np.max(tmp_dicom_array) <= 255
+        scipy.misc.imsave(os.path.join(train_images_dir, each_train_dicom.split("/")[-1].replace("dcm", "png")),
+                          tmp_dicom_array)
+
+        
+def dicom_to_png_conversion_test(list_of_test_dicoms):
+   
+    for i, each_test_dicom in enumerate(list_of_test_dicoms): 
+        sys.stdout.write("DICOM to PNG conversion: {}/{} ...\r".format(i+1, len(list_of_test_dicoms)))
+        sys.stdout.flush() 
+        tmp_dicom_array = pydicom.read_file(each_test_dicom).pixel_array
+        assert np.min(tmp_dicom_array) >= 0 & np.max(tmp_dicom_array) <= 255
+        scipy.misc.imsave(os.path.join(test_images_dir, each_test_dicom.split("/")[-1].replace("dcm", "png")),
+                          tmp_dicom_array) 
+    
+    
+from multiprocessing import Pool, cpu_count
 
 
-for i, each_test_dicom in enumerate(list_of_test_dicoms): 
-    sys.stdout.write("DICOM to PNG conversion: {}/{} ...\r".format(i+1, len(list_of_test_dicoms)))
-    sys.stdout.flush() 
-    tmp_dicom_array = pydicom.read_file(each_test_dicom).pixel_array
-    assert np.min(tmp_dicom_array) >= 0 & np.max(tmp_dicom_array) <= 255
-    scipy.misc.imsave(os.path.join(test_images_dir, each_test_dicom.split("/")[-1].replace("dcm", "png")),
-                      tmp_dicom_array)
+######### Parallelized version
+### train 
 
+n=round(len(list_of_train_dicoms)/cpu_count())
+files_train= [list_of_train_dicoms[i:i + n] for i in range(0, len(list_of_train_dicoms), n)]
 
+with Pool(processes=cpu_count()) as pool:  
+    pool.map(dicom_to_png_conversion_train, files_train)
 
+### test 
+
+n=round(len(list_of_test_dicoms)/cpu_count())
+files_test= [list_of_test_dicoms[i:i + n] for i in range(0, len(list_of_test_dicoms), n)]
+
+with Pool(processes=cpu_count()) as pool:  
+    pool.map(dicom_to_png_conversion_train, files_test)
